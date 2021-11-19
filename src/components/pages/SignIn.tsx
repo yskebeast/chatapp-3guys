33f9a8fc4a13e2ebb55d7a11f431970af8d098a9
rapwwerock5091@gmail.com <rapwwerock5091@gmail.com>
2021-11-19 17:25:02 +0900

    Hooksでの認証機能を実装

--- src/components/pages/SignIn.tsx
+++ src/components/pages/SignIn.tsx
@@ -1,23 +1,63 @@
-import { VFC } from "react-router/node_modules/@types/react";
+import { ChangeEvent, useState, VFC } from "react";
 import { useHistory } from "react-router";
 import { Link } from "react-router-dom";
+import { auth } from "../../firebase";
+import { signInWithEmailAndPassword } from "@firebase/auth";
+
 import { AuthButton } from "../atoms/button/AuthButton";
+
+const center = {
+  width: "300px",
+  margin: "0 auto",
+};
 
 export const SignIn: VFC = () => {
   const history = useHistory();
-  const handleSignIn = () => {
-    history.push("/home")
-};
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  const handleChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+  const handleChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSignIn = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    await signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        history.push("/home");
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  };
 
   return (
-    <div>
+    <div style={center}>
       <h1>ログイン</h1>
+      {error && <p>{error}</p>}
       <form onSubmit={handleSignIn}>
         <div>
-          <input name="email" type="email" placeholder="メールアドレス" />
+          <input
+            name="email"
+            type="email"
+            placeholder="メールアドレス"
+            value={email}
+            onChange={handleChangeEmail}
+          />
         </div>
         <div>
-          <input name="password" type="password" placeholder="パスワード" />
+          <input
+            name="password"
+            type="password"
+            placeholder="パスワード"
+            value={password}
+            onChange={handleChangePassword}
+          />
         </div>
         <div>
           <AuthButton>ログイン</AuthButton>
--- src/components/pages/SignUp.tsx
+++ src/components/pages/SignUp.tsx
@@ -1,31 +1,102 @@
-import { VFC } from "hoist-non-react-statics/node_modules/@types/react";
+import { ChangeEvent, useState, VFC } from "react";
 import { useHistory } from "react-router";
 import { Link } from "react-router-dom";
+import { auth, db } from "../../firebase";
+import { createUserWithEmailAndPassword } from "@firebase/auth";
+
 import { AuthButton } from "../atoms/button/AuthButton";
+import { doc, setDoc } from "@firebase/firestore";
+
+const center = {
+  width: "300px",
+  margin: "0 auto",
+};
 
 export const SignUp: VFC = () => {
   const history = useHistory();
-  const handleSignUp = () => {
-    history.push("/home");
+
+  const [name, setName] = useState<string>("");
+  const [selfIntro, setSelfIntro] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  const uid = auth.currentUser?.uid;
+
+  const handleChangeName = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+  const handleChangeSelfIntro = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setSelfIntro(e.target.value);
+  };
+  const handleChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+  const handleChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSignUp = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        history.push("/home");
+        setDoc(doc(db, "users", `${uid}`), {
+          name: name,
+          selfIntro: selfIntro,
+        });
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
   };
 
   return (
-    <div>
+    <div style={center}>
       <h1>アカウントを作成</h1>
+      {error && <p>{error}</p>}
       <form onSubmit={handleSignUp}>
         <div>
-          <input name="name" type="text" placeholder="名前" />
+          <input
+            name="name"
+            type="text"
+            placeholder="名前"
+            value={name}
+            onChange={handleChangeName}
+          />
         </div>
         <div>
-          <input name="email" type="email" placeholder="メールアドレス" />
+          <textarea
+            name="text"
+            placeholder="自己紹介"
+            value={selfIntro}
+            onChange={handleChangeSelfIntro}
+          />
         </div>
         <div>
-          <input name="password" type="password" placeholder="パスワード" />
+          <input
+            name="email"
+            type="email"
+            placeholder="メールアドレス"
+            value={email}
+            onChange={handleChangeEmail}
+          />
         </div>
         <div>
-      <AuthButton>登録</AuthButton>
+          <input
+            name="password"
+            type="password"
+            placeholder="パスワード"
+            value={password}
+            onChange={handleChangePassword}
+          />
         </div>
-        <p>ログインは<Link to='/'>こちら</Link>から</p>
+        <div>
+          <AuthButton>登録</AuthButton>
+        </div>
+        <p>
+          ログインは<Link to="/">こちら</Link>から
+        </p>
       </form>
     </div>
   );
