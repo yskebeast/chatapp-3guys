13ee4e246546ec4c683ed7f2b6fe3d3c57837466
rapwwerock5091@gmail.com <rapwwerock5091@gmail.com>
2021-11-19 18:01:08 +0900

    未設定

--- src/providers/AuthUserProvider.tsx
+++ src/providers/AuthUserProvider.tsx
@@ -1,12 +1,28 @@
-import {createContext, ReactNode} from "react"
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { auth } from "../firebase";
+import { onAuthStateChanged } from "@firebase/auth";
 
-export const AuthUserContext = createContext<any>({})
-
-export const AuthUserProvider = (props:{children: ReactNode}) => {
-  const {children} = props
-  return (
-    <AuthUserContext.Provider value>
-      {children}
-    </AuthUserContext.Provider>
-  )
+type ContextState = {
+  user: string | null;
 }
+
+export const AuthUserContext = createContext({} as ContextState);
+
+export const AuthUserProvider = (props: { children: ReactNode }) => {
+  const { children } = props;
+
+  const [user, setUser] = useState<any>("");
+
+  useEffect(() => {
+    const unsubscribed = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+    return () => {
+      unsubscribed();
+    };
+  }, []);
+
+  return (
+    <AuthUserContext.Provider value={user}>{children}</AuthUserContext.Provider>
+  );
+};
