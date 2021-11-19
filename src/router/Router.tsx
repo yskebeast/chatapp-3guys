8828824cbb19748be54692f0f4ef0d338fe9f3f4
rapwwerock5091@gmail.com <rapwwerock5091@gmail.com>
2021-11-19 11:33:44 +0900

    ルーター作成

---
+++ src/router/Router.tsx
@@ -0,0 +1,28 @@
+import { Route, Switch } from "react-router";
+
+import { AuthUserProvider } from "../providers/AuthUserProvider";
+import { SignIn } from "../components/pages/SignIn";
+import { SignUp } from "../components/pages/SignUp";
+import { Home } from "../components/pages/Home";
+import { Page404 } from "../components/pages/Page404";
+
+export const Router = () => {
+  return (
+    <Switch>
+      <AuthUserProvider>
+        <Route exact path="/">
+          <SignIn />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+      </AuthUserProvider>
+      <Route path="*">
+        <Page404 />
+      </Route>
+    </Switch>
+  );
+};
