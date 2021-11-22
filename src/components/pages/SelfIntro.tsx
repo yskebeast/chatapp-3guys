6113231aa03968674bf6f8c344beefeb7aadbc84
rapwwerock5091@gmail.com <rapwwerock5091@gmail.com>
2021-11-22 16:26:42 +0900

    自己紹介ページ追加

---
+++ src/components/pages/SelfIntro.tsx
@@ -0,0 +1,50 @@
+import { ThemeProvider } from "@emotion/react";
+import { createTheme, TextField,  } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import { Box,  } from "@mui/system";
+import { ChangeEvent, memo, VFC } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useAppDispatch } from "../../app/hooks";
+import { selectUser } from "../../features/userSlice";
+
+export const SelfIntro: VFC = memo(() => {
+  const loginUser = useSelector(selectUser);
+  const dispach = useAppDispatch();
+
+  return (
+    // <ThemeProvider theme={theme}>
+      <Box sx={{ width: "60%", marginX: "auto", textAlign: "center" }}>
+        <h1>プロフィールを編集</h1>
+        <div>
+          <img
+            style={{ width: "150px", height: "150px", background: "white" }}
+            src=""
+            alt=""
+          />
+        </div>
+        <div>
+          <p style={{ textAlign: "left" }}>名前</p>
+          <TextField
+            sx={{ width: "100%" }}
+            type="text"
+            value={loginUser.username}
+            // onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            //   dispach(loginUser({username: e.target.value}))
+            // }
+          />
+        </div>
+        <div>
+          <p style={{ textAlign: "left" }}>自己紹介</p>
+          <TextField
+            sx={{ width: "100%" }}
+            type="text"
+            value={loginUser.selfIntro}
+            // onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            //   dispach(loginUser.selfIntro(e.target.value))
+            // }
+          />
+        </div>
+      </Box>
+    // </ThemeProvider>
+  );
+});
