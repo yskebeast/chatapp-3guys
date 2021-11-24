a0fc3c648763787cb59f9dc3252cbe3aa481f9d1
rapwwerock5091@gmail.com <rapwwerock5091@gmail.com>
2021-11-24 19:00:55 +0900

    prettier適応

--- src/features/userSlice.ts
+++ src/features/userSlice.ts
@@ -2,18 +2,18 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../app/store";
 
 export type certificationState = {
-  user:{username: string, uid: string, selfIntro: string,
-}}
+  user: { username: string; uid: string; selfIntro: string };
+};
 
 export type certificationState2 = {
-  username: string,
-  uid: string,
-  selfIntro: string,
-}
+  username: string;
+  uid: string;
+  selfIntro: string;
+};
 
 const initialState: certificationState = {
-  user: {username: "", uid: "", selfIntro: "",}
-} 
+  user: { username: "", uid: "", selfIntro: "" },
+};
 
 export const userSlice = createSlice({
   name: "user",
@@ -22,12 +22,12 @@ export const userSlice = createSlice({
     login: (state, action: PayloadAction<certificationState2>) => {
       state.user = action.payload;
     },
-    logout:(state) => {
-      state.user = {username:"", uid: "", selfIntro:""}
-    }
+    logout: (state) => {
+      state.user = { username: "", uid: "", selfIntro: "" };
+    },
   },
 });
 
 export const { login, logout } = userSlice.actions;
-export const selectUser = (state: RootState) => state.user.user
+export const selectUser = (state: RootState) => state.user.user;
 export default userSlice.reducer;
--- src/providers/AuthUserProvider.tsx
+++ src/providers/AuthUserProvider.tsx
@@ -8,13 +8,12 @@ import {
 import { useHistory } from "react-router";
 import { auth } from "../firebase";
 import { onAuthStateChanged } from "@firebase/auth";
-import { db } from "../firebase"
+import { db } from "../firebase";
 import { doc, getDoc } from "firebase/firestore";
 import { useSelector, useDispatch } from "react-redux";
 import { selectUser, login, logout } from "../features/userSlice";
-import { store } from '../app/store';
-import { Provider } from 'react-redux';
-
+import { store } from "../app/store";
+import { Provider } from "react-redux";
 
 const AuthUserContext = createContext({});
 
@@ -27,9 +26,9 @@ export const AuthUserProvider = (props: { children: ReactNode }) => {
   const dispatch = useDispatch();
   const history = useHistory();
 
-  const getDocument = async(user:any) => {
+  const getDocument = async (user: any) => {
     const docRef = doc(db, "users", user.uid);
-    const docSnap =  await getDoc(docRef);
+    const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
       dispatch(
         login({
@@ -37,21 +36,21 @@ export const AuthUserProvider = (props: { children: ReactNode }) => {
           uid: user.uid,
           selfIntro: docSnap.data().selfIntro,
         })
-      )
+      );
     } else {
       // doc.data() will be undefined in this case
       console.log("No such document!");
     }
-  }
+  };
 
   useEffect(() => {
     const unsubscribed = onAuthStateChanged(auth, (user) => {
       if (user) {
-        getDocument(user)
+        getDocument(user);
 
         history.push(`/home/${auth.currentUser?.uid}`);
       } else {
-        dispatch(logout())
+        dispatch(logout());
         history.push("/");
       }
     });
@@ -60,8 +59,5 @@ export const AuthUserProvider = (props: { children: ReactNode }) => {
     };
   }, [history]);
 
-  return (
-    <Provider store={store}>{children}</Provider>
-  );
+  return <Provider store={store}>{children}</Provider>;
 };
-
