5405e0a8ab6b695775743a4decd89d80358d81ae
rapwwerock5091@gmail.com <rapwwerock5091@gmail.com>
2021-11-24 17:54:23 +0900

    掲示板ページとして制作

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,14 +1,42 @@
-import { memo, VFC } from "react";
+import { ChangeEvent, memo, useEffect, useState, VFC } from "react";
 import { useHistory } from "react-router";
-import { auth } from "../../firebase";
+import { auth, db } from "../../firebase";
 import { signOut } from "@firebase/auth";
 
 import { useSelector } from "react-redux";
 import { selectUser } from "../../features/userSlice";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+} from "@firebase/firestore";
+
+import "../../App.css";
 
 export const Home: VFC = memo(() => {
   const history = useHistory();
   const loginUser = useSelector(selectUser);
+
+  const [messages, setMessages] = useState<Array<any>>([]);
+  const [tweet, setTweet] = useState("");
+  const [like, setLike] = useState<number>(0);
+  const [f, sf] = useState(false);
+  const [close, setClose] = useState(false);
+
+  useEffect(() => {
+    const r = collection(db, "board", "common", "messages");
+    const q = query(r, orderBy("time", "desc"));
+    onSnapshot(q, (querySnapshot) => {
+      const arr: Array<any> = [];
+      querySnapshot.forEach((doc) => {
+        arr.push(doc.data().tweet);
+      });
+      setMessages(arr);
+    });
+  }, []);
 
   const handleLogout = () => {
     signOut(auth).then(() => {
@@ -20,16 +48,78 @@ export const Home: VFC = memo(() => {
     history.push(`/selfintro/${auth.currentUser?.uid}`);
   };
 
+  const handleLike = (index: any) => {};
+
+  const handleSend = (index: any) => {
+    setClose(!close)
+    if (!f) {
+      sf(!false);
+    } else {
+      sf(false);
+    }
+  };
+
+  const handleReply = () => {};
+
+  const handleTweet = async () => {
+    const timestamp = serverTimestamp();
+    const newMessages = [...messages, tweet];
+    setMessages(newMessages);
+    const ref = collection(db, "board", "common", "messages");
+    await addDoc(ref, {
+      tweet: tweet,
+      time: timestamp,
+      user: loginUser.uid,
+      name: loginUser.username,
+    });
+    setTweet("");
+  };
+
   return (
     <div>
-      <h1>メイン画面</h1>
-      <p>ユーザーID:{loginUser.uid}</p>
-      <p>ユーザーネーム：{loginUser.username}</p>
-      <p>自己紹介：{loginUser.selfIntro}</p>
-      <button onClick={handleLogout}>ログアウト</button>
-      <div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
+      >
+        <h1>{loginUser.username}</h1>
         <button onClick={handleIntro}>自己紹介</button>
+        <button onClick={handleLogout}>ログアウト</button>
       </div>
+      <ul>
+        {messages.map((message, index) => {
+          return (
+            <div key={index}>
+              <li>{message}</li>
+              {f && (
+                <div>
+                  <ul>
+                    <li>hello</li>
+                  </ul>
+                  <div>
+                    <input type="text" />
+                    <button onClick={handleReply}>送信</button>
+                  </div>
+                </div>
+              )}
+              <button onClick={() => handleLike(index)}>いいね</button>
+              <button onClick={() => handleSend(index)}>
+                {close ? <p>返信</p> : <p>閉じる</p>}
+              </button>
+            </div>
+          );
+        })}
+      </ul>
+      <input
+        type="text"
+        value={tweet}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setTweet(e.target.value)
+        }
+      />
+      <button onClick={handleTweet}>追加</button>
     </div>
   );
 });
