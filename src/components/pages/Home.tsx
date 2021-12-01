8c6ea06a01d3533eda0d484d53aaf24ad16c7da8
haru <haruki1472@icloud.com>
2021-12-01 16:15:46 +0900

    userlist リンク動かない

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,16 +1,32 @@
 import { memo, VFC } from "react";
 import { useHistory } from "react-router";
-import { auth } from "../../firebase";
+import { auth, db } from "../../firebase";
 import { signOut } from "@firebase/auth";
 
-
-import { useAuthUserContext } from "../../providers/AuthUserProvider";
-import { collection, getDocs, query } from "@firebase/firestore";
-import { useSelector, useDispatch } from "react-redux";
-import { selectUser, login, logout } from "../../features/userSlice";
-import Header from "../organisms/Header";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../features/userSlice";
+import { collection, onSnapshot, query } from "firebase/firestore";
 
 export const Home: VFC = memo(() => {
+  const get = () => {
+    const q = query(collection(db, "board"));
+    onSnapshot(q, (querySnapshot) => {
+      const cities: any = []; //citiesを配列で宣言
+      querySnapshot.forEach((doc) => {
+        //この関数が値を取得する関数になっている。
+        cities.push(doc.data());
+        console.log("関数起動");
+        console.log(doc.data());
+        console.log("関数起動");
+      });
+      console.log("Current cities in CA: ", cities.join(", "));
+    });
+  };
+
+  const buttonUser = () => {
+    history.push("./userlist");
+  };
+
   const history = useHistory();
   const loginUser = useSelector(selectUser);
 
@@ -26,7 +42,8 @@ export const Home: VFC = memo(() => {
 
   return (
     <div>
-      <Header />
+      <button onClick={get}></button>
+      <button onClick={buttonUser}>userlinkへのボタン</button>
       <h1>メイン画面</h1>
       <p>ユーザーID:{loginUser.uid}</p>
       <p>ユーザーネーム：{loginUser.username}</p>
--- src/components/pages/UserList.tsx
+++ src/components/pages/UserList.tsx
@@ -1,13 +1,43 @@
 import * as React from "react";
+import { useState } from "react";
 import ImageList from "@mui/material/ImageList";
 // import ImageListItem from "@mui/material/ImageListItem";
 // import ImageListItemBar from "@mui/material/ImageListItemBar";
 import Card from "../molecules/Card";
+import { collection, onSnapshot, query } from "firebase/firestore";
+import { db } from "../../firebase";
 
-export default function TitlebarBelowImageList() {
+export const UserList = () => {
+  var namedata: any = [];
+  let r;
+  r = 0;
+  const [itemData, setitemData] = useState<any>([
+    {
+      img: "",
+      name: "",
+    },
+  ]);
+
+  const q = query(collection(db, "board"));
+
+  //1.ユーザーを取得する。currenUser nullを指定。2.取得したユーザーを変数に代入。3.取得したユーザーの数だけ配列を宣言。4.forで取得したユーザー数分だけ回しながら、配列に代入。
+
+  onSnapshot(q, (querySnapshot) => {
+    const cities: any = []; //citiesを配列で宣言
+    querySnapshot.forEach((doc) => {
+      // この関数が値を取得する関数になっている。
+      // itemData["title"].push(doc.data().name);
+      setitemData({ img: doc.data().img, name: doc.data() });
+
+      console.log("関数起動");
+      console.log(doc.data().name);
+      // let length1 = Object.keys(namedata).length;
+    });
+  });
+  console.log(itemData);
   return (
     <ImageList sx={{ mx: "auto", width: 1000 }} cols={3}>
-      {itemData.map((item) => {
+      {itemData.map((item: any) => {
         // return console.log(item.title);
         return <Card />;
       })}
@@ -29,23 +59,6 @@ export default function TitlebarBelowImageList() {
       ))} */}
     </ImageList>
   );
-}
+};
 
-const itemData = [
-  {
-    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-    title: "ooba",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-    title: "kakehi",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
-    title: "tani",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-    title: "munakata",
-  },
-];
+// userのドキュメントを取得
