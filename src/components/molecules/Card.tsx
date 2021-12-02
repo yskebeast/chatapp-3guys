63c24d4d4e952bd3ab36fb55e056110276836222
haru <haruki1472@icloud.com>
2021-12-02 15:59:14 +0900

    user一覧完成しました。

--- src/components/molecules/Card.tsx
+++ src/components/molecules/Card.tsx
@@ -1,78 +1,41 @@
-import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 import Avatar from "../atoms/avatar/Avatar";
-import { collection, query, where, onSnapshot, doc } from "firebase/firestore";
-import { db } from "../../firebase";
-import { render } from "@testing-library/react";
+import "./Card.css";
 
-export default function ActionAreaCard() {
-  //   const q = query(collection(db, "board"));
-  //   onSnapshot(q, (querySnapshot) => {
-  //     const cities: any = []; //citiesを配列で宣言
-  //     querySnapshot.foEach((doc) => {
-  //       //この関数が値を取得する関数になっている。
-  //       cities.push(doc.data());
-  //       console.log("関数起動");
-  //       console.log(doc.data());
-  //       console.log(doc.data().name);
-  //       console.log("関数起動");
-  //     });
-  //     console.log("Current cities in CA: ", cities.join(", "));
-  //   });
+type PropsType = {
+  img: string;
+  name: string;
+};
 
+export default function ActionAreaCard(props: PropsType) {
+  const { img, name } = props;
+  console.log(img);
+  console.log(name);
   return (
-    <Card sx={{ maxWidth: 250 }}>
+    <Card className="Card__card" sx={{ maxWidth: 250 }}>
       <Avatar />
       <CardActionArea>
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            ここに名前を挿入したい
+          <Typography
+            className="Card__card__font"
+            gutterBottom
+            variant="h5"
+            component="div"
+          >
+            {name}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
-            自己紹介とかを挿入したい
+          <Typography
+            className="Card__card__font-selfIntro"
+            variant="body2"
+            color="text.secondary"
+          >
+            {img}
           </Typography>
         </CardContent>
       </CardActionArea>
     </Card>
   );
 }
-
-// props
-
-// class Square extends React.Component {
-//   render() {
-//     return <button className="square">{this.props.value}</button>;
-//   }
-// }
-
-// class Board extends React.Component {
-//   renderSquare(i) {
-//     return <Square value={i} />; //ポイント1
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <div className="status">{status}</div>
-//         <div className="board-row">
-//           {this.renderSquare(0)}
-//           {this.renderSquare(1)}
-//           {this.renderSquare(2)}
-//         </div>
-//         <div className="board-row">
-//           {this.renderSquare(3)}
-//           {this.renderSquare(4)}
-//           {this.renderSquare(5)}
-//         </div>
-//         <div className="board-row">
-//           {this.renderSquare(6)}
-//           {this.renderSquare(7)}
-//           {this.renderSquare(8)}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
--- src/components/pages/UserList.tsx
+++ src/components/pages/UserList.tsx
@@ -1,51 +1,60 @@
 import * as React from "react";
 import ImageList from "@mui/material/ImageList";
-// import ImageListItem from "@mui/material/ImageListItem";
-// import ImageListItemBar from "@mui/material/ImageListItemBar";
 import Card from "../molecules/Card";
+import { useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import Header from "../organisms/Header";
+import { db } from "../../firebase";
+import "./UserList.css";
+
+type itemType = {
+  img: string;
+  name: string;
+};
 
 export const UserList = () => {
+  const itemData: itemType[] = [];
+  const [data, setData] = useState<itemType[]>();
+
+  React.useEffect(() => {
+    const yobidasi = async () => {
+      const querySnapshot = await getDocs(collection(db, "users"));
+      querySnapshot.forEach((doc) => {
+        itemData.push({ img: doc.data().selfIntro, name: doc.data().name });
+      });
+      setData(itemData);
+    };
+    yobidasi();
+  }, []);
+
+  const [loading, setLoading] = useState(true);
+  React.useEffect(() => {
+    if (itemData !== undefined) {
+      setLoading(false);
+    }
+  }, [itemData]);
   return (
-    <ImageList sx={{ mx: "auto", width: 1000 }} cols={3}>
-      {itemData.map((item) => {
-        // return console.log(item.title);
-        return <Card />;
-      })}
-
-      {/* home.tsx内のスライスからデータを取れるやつがあるので、取得すればいい。 */}
-      {/* userに変化があったらdispatchで変更、firebaseで更新。 */}
-      {/* 複数のドキュメント取得→get clickしたらcollectionのdb,"users"を選択　getodocksをしたら変数の前ドキュメントを取得するfor */}
-
-      {/* {itemData.map((item) => (
-        <ImageListItem key={item.img}>
-          <img
-            src={`${item.img}?w=248&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
-          />
-          <ImageListItemBar title={item.title} position="below" />
-        </ImageListItem>
-      ))} */}
-    </ImageList>
+    <div className="UserList__main">
+      <Header />
+      {loading ? (
+        <p>naiyo</p>
+      ) : (
+        <ImageList
+          className="UserList__ImageList"
+          sx={{ mx: "auto", width: 1000 }}
+          cols={3}
+        >
+          {data !== undefined &&
+            data.map((item) => {
+              const { img, name } = item;
+              return (
+                <>
+                  <Card img={img} name={name} />
+                </>
+              );
+            })}
+        </ImageList>
+      )}
+    </div>
   );
-}
-
-const itemData = [
-  {
-    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-    title: "ooba",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-    title: "kakehi",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
-    title: "tani",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-    title: "munakata",
-  },
-];
+};
